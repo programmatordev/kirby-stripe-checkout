a99378f6260dfd70b5df8108fe1f5c52d0c83bcb
André Pimpão <a.pimpao@programmator.dev>
2024-09-10 17:48:43 +0100

    feat: added base product fields and pages

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
 
-use Kirby\Cms\App as Kirby;
+use Kirby\Cms\App;
 
 @include_once __DIR__ . '/vendor/autoload.php';
 
-Kirby::plugin('programmatordev/kirby-stripe-checkout', [
-
+App::plugin('programmatordev/kirby-stripe-checkout', [
+    'blueprints' => [
+        // fields
+        'stripe.checkout.fields/price' => __DIR__ . '/blueprints/fields/price.yml',
+        'stripe.checkout.fields/tax' => __DIR__ . '/blueprints/fields/tax.yml',
+        'stripe.checkout.fields/cover' => __DIR__ . '/blueprints/fields/cover.yml',
+        'stripe.checkout.fields/options' => __DIR__ . '/blueprints/fields/options.yml',
+        // pages
+        'stripe.checkout.pages/product' => __DIR__ . '/blueprints/pages/product.yml',
+        'stripe.checkout.pages/product-options' => __DIR__ . '/blueprints/pages/product-options.yml',
+    ]
 ]);
