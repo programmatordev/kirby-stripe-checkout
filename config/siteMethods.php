11ead374b9adaa19ad7d7c357e8173aaf1d8cce9
André Pimpão <a.pimpao@programmator.dev>
2024-10-24 12:02:20 +0100

    feat: add site method to get allowed Stripe countries url

--- config/siteMethods.php
+++ config/siteMethods.php
@@ -9,6 +9,16 @@ return [
         $currency = strtoupper(option('programmatordev.stripe-checkout.currency'));
         return Currencies::getSymbol($currency);
     },
+    'stripeCountriesUrl' => function(?string $locale = null): string
+    {
+        $url = sprintf('%s/api/stripe/countries', kirby()->url());
+
+        if ($locale !== null) {
+            $url = sprintf('%s?locale=%s', $url, $locale);
+        }
+
+        return $url;
+    },
     'stripeCheckoutUrl' => function(): string
     {
         /** @var Site $this */
