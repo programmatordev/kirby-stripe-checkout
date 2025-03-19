a9fd1055fee13f68baaa3ea28f85ee1fef798749
André Pimpão <a.pimpao@programmator.dev>
2025-03-19 11:33:57 +0000

    chore: move plugin config to multiple files

---
+++ config/blueprints.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // fields
+    'stripe-checkout.fields/price' => __DIR__ . '/../blueprints/fields/price.yml',
+    'stripe-checkout.fields/cover' => __DIR__ . '/../blueprints/fields/cover.yml',
+    // sections
+    'stripe-checkout.sections/orders' => __DIR__ . '/../blueprints/sections/orders.yml',
+    // pages
+    'stripe-checkout.pages/orders' => __DIR__ . '/../blueprints/pages/orders.yml',
+    'stripe-checkout.pages/order' => __DIR__ . '/../blueprints/pages/order.yml',
+    'stripe-checkout.pages/checkout-settings' => __DIR__ . '/../blueprints/pages/checkout-settings.yml',
+];
