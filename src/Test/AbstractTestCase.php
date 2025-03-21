c9a886bd4e8026ac81287d688c6f68f128dacdda
André Pimpão <a.pimpao@programmator.dev>
2025-03-21 12:51:44 +0000

    fix: phpunit 11 support risky tests

--- src/Test/AbstractTestCase.php
+++ src/Test/AbstractTestCase.php
@@ -3,6 +3,7 @@
 namespace ProgrammatorDev\StripeCheckout\Test;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Runner\ErrorHandler;
 
 class AbstractTestCase extends TestCase
 {
@@ -11,5 +12,52 @@ class AbstractTestCase extends TestCase
         parent::setUp();
 
         kirby()->impersonate('kirby');
+    }
+
+    protected function tearDown(): void
+    {
+        parent::tearDown();
+
+        $this->flushHandlerState();
+    }
+
+    /**
+     * Flush the bootstrapper global handlers state.
+     * https://github.com/symfony/symfony/issues/53812#issuecomment-1958859357
+     */
+    private function flushHandlerState(): void
+    {
+        while (true) {
+            $previousHandler = set_exception_handler(static fn() => null);
+
+            restore_exception_handler();
+
+            if ($previousHandler === null) {
+                break;
+            }
+
+            restore_exception_handler();
+        }
+
+        while (true) {
+            $previousHandler = set_error_handler(static fn() => null);
+
+            restore_error_handler();
+
+            if ($previousHandler === null) {
+                break;
+            }
+
+            restore_error_handler();
+        }
+
+        if (class_exists(ErrorHandler::class)) {
+            $instance = ErrorHandler::instance();
+
+            if ((fn() => $this->enabled ?? false)->call($instance)) {
+                $instance->disable();
+                $instance->enable();
+            }
+        }
     }
 }
