32667b6c116120bcba6b46282c82e7cca79a72a2
André Pimpão <a.pimpao@programmator.dev>
2025-03-27 12:44:19 +0000

    tests: update MoneyFormatter tests

--- tests/MoneyFormatterTest.php
+++ tests/MoneyFormatterTest.php
@@ -15,18 +15,14 @@ class MoneyFormatterTest extends AbstractTestCase
     public function testFromMinorUnit(): void
     {
         $this->assertSame(1000.0, MoneyFormatter::fromMinorUnit(100000, 'EUR'));
+        $this->assertSame('1,000.00', MoneyFormatter::fromMinorUnit(100000, 'EUR', true));
         $this->assertSame(1000, MoneyFormatter::fromMinorUnit(1000, 'JPY'));
+        $this->assertSame('1,000', MoneyFormatter::fromMinorUnit(1000, 'JPY', true));
     }
 
     public function testFormat(): void
     {
-        $this->assertSame('€ 1,000.00', MoneyFormatter::format(1000, 'EUR'));
-        $this->assertSame('¥ 1,000', MoneyFormatter::format(1000, 'JPY'));
-    }
-
-    public function testFormatFromMinorUnit(): void
-    {
-        $this->assertSame('€ 1,000.00', MoneyFormatter::formatFromMinorUnit(100000, 'EUR'));
-        $this->assertSame('¥ 1,000', MoneyFormatter::formatFromMinorUnit(1000, 'JPY'));
+        $this->assertSame('1,000.00', MoneyFormatter::format(1000, 'EUR'));
+        $this->assertSame('1,000', MoneyFormatter::format(1000, 'JPY'));
     }
 }
