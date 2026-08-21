a405a25968a39f9f49f5d9a8ab3b953050af9c2b
André Pimpão <a.pimpao@programmator.dev>
2026-08-21 10:29:19 +0100

    fix(plugin): register explicit version metadata

--- index.php
+++ index.php
@@ -5,24 +5,28 @@ use Kirby\Cms\App;
 @include_once __DIR__ . '/vendor/autoload.php';
 @include_once __DIR__ . '/helpers.php';
 
-App::plugin('programmatordev/stripe-checkout', [
-    'options' => [
-        'stripePublicKey' => null,
-        'stripeSecretKey' => null,
-        'stripeWebhookSecret' => null,
-        'uiMode' => 'hosted',
-        'currency' => 'EUR',
-        'returnPage' => null,
-        'successPage' => null,
-        'cancelPage' => null,
-        'ordersPage' => 'orders',
-        'settingsPage' => 'checkout-settings',
-        'cartSnippet' => null,
-        'translations' => []
+App::plugin(
+    name: 'programmatordev/stripe-checkout',
+    extends: [
+        'options' => [
+            'stripePublicKey' => null,
+            'stripeSecretKey' => null,
+            'stripeWebhookSecret' => null,
+            'uiMode' => 'hosted',
+            'currency' => 'EUR',
+            'returnPage' => null,
+            'successPage' => null,
+            'cancelPage' => null,
+            'ordersPage' => 'orders',
+            'settingsPage' => 'checkout-settings',
+            'cartSnippet' => null,
+            'translations' => []
+        ],
+        'blueprints' => require __DIR__ . '/config/blueprints.php',
+        'translations' => require __DIR__ . '/config/translations.php',
+        'siteMethods' => require __DIR__ . '/config/siteMethods.php',
+        'routes' => require __DIR__ . '/config/routes.php',
+        'api' => require __DIR__ . '/config/api.php',
     ],
-    'blueprints' => require __DIR__ . '/config/blueprints.php',
-    'translations' => require __DIR__ . '/config/translations.php',
-    'siteMethods' => require __DIR__ . '/config/siteMethods.php',
-    'routes' => require __DIR__ . '/config/routes.php',
-    'api' => require __DIR__ . '/config/api.php',
-]);
+    version: '0.7.0'
+);
